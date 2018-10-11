b903e8d77b608845d8ce41e8afdc895c839fe84b
luquio <luquio@icloud.com>
2018-10-11 16:50:39 -0300

    Added improvements and nasty language

--- 02_ultrasonic/02_ultrasonic.ino
+++ 02_ultrasonic/02_ultrasonic.ino
@@ -1,6 +1,7 @@
 // Setup Ultrasonic Sensor pins
 #define trigPin 8
 #define echoPin 9
+#define piezoPin 12
 
 #include <Servo.h>
 Servo myservo;
@@ -19,12 +20,28 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int dist = distance();
+  //DistanceSound();
+  /*int dist = distance();
   if(dist < 50)
   {
     MoveHeadFast();
   }
-    
+  if(dist < 10)
+  {
+    alarm();
+  }*/
+}
+
+void DistanceSound()
+{
+  int dist = distance();
+  
+  if(dist > 30)
+    return;
+
+  tone(piezoPin, 3000, 50);
+  delay(10 * dist);
+  Serial.println(dist);
 }
 
 int angle = 0;
@@ -66,4 +83,72 @@ int distance() {
   if(distance < 0)
     distance = distance * -1;
   return distance;
+}
+
+//------------------------------------- MORSE CODE -------------------------------------//
+
+int note = 2500;
+int mShort = 50;
+
+void alarm()
+{
+  int mLong = mShort * 3;
+  
+  int delayBetweenNotes = mShort * 1;
+  int delayBetweenLeters = mShort * 3;
+  int delayBetweenWords = mShort * 7;
+
+  Serial.println("P");
+  
+  // P
+  tone(piezoPin, note, mShort);
+  delay(delayBetweenNotes + mShort);
+  tone(piezoPin, note, mLong);
+  delay(delayBetweenNotes + mLong);
+  tone(piezoPin, note, mLong);
+  delay(delayBetweenNotes + mLong);
+  tone(piezoPin, note, mShort);
+  delay(delayBetweenNotes + mShort);
+
+  // letter pause
+  delay(delayBetweenLeters);
+
+  Serial.println("U");
+
+  // U
+  tone(piezoPin, note, mShort);
+  delay(delayBetweenNotes + mShort);
+  tone(piezoPin, note, mShort);
+  delay(delayBetweenNotes + mShort);
+  tone(piezoPin, note, mLong);
+  delay(delayBetweenNotes + mLong);
+
+  // letter pause
+  delay(delayBetweenLeters);
+
+  Serial.println("T");
+
+  // T
+  tone(piezoPin, note, mLong);
+  delay(delayBetweenNotes + mLong);
+
+  // letter pause
+  delay(delayBetweenLeters);
+
+  Serial.println("O");
+  
+  // O
+  tone(piezoPin, note, mLong);
+  delay(delayBetweenNotes + mLong);
+  tone(piezoPin, note, mLong);
+  delay(delayBetweenNotes + mLong);
+  tone(piezoPin, note, mLong);
+  delay(delayBetweenNotes + mLong);
+
+  // letter pause
+  delay(delayBetweenLeters);
+
+  // word pause
+  delay(delayBetweenWords);
+
 }
